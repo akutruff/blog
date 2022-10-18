a88daa2d983e4fa44bbf560a6f37b1099254fc96
akutruff <874049+akutruff@users.noreply.github.com>
2022-10-18 16:35:35 +0000

    see if this fixes it

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    assetPrefix: isProd ? "/blog/" : "",
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
