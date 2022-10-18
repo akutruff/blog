89675b35c5e9c27600d3e53f3837675294d59d81
akutruff <874049+akutruff@users.noreply.github.com>
2022-10-18 16:40:21 +0000

    try more patthing

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
-const isProd = process.env.NODE_ENV === "production";
+const pathPrefix = process.env.NODE_ENV === 'production'
+  ? '/blog'
+  : '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    assetPrefix: isProd ? "/blog/" : "",
-    images: {
-        unoptimized: true,
-    },
+    assetPrefix: pathPrefix,
+    env: {
+        pathPrefix
+    }
 }
 
 module.exports = nextConfig
